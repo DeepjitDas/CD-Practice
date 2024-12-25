b2552b0d76471c354df8bb8acbeae3d06ab490e0
DeepjitDas <deepjit.das1995@gmail.com>
2024-12-25 15:38:33 +0530

    Test commit for gitHub action to run

--- CD Practice/Controllers/WeatherForecastController.cs
+++ CD Practice/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace CD_Practice.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Test Change"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
